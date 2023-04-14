b06bc1179b8cbd0f4fac3521448fc91811ab5dd0
woochangmin <rebuilding33@gmail.com>
2023-04-14 20:24:57 +0900

    초기 커밋.

---
+++ Commit_Test1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Commit_Test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = Console.ReadLine();                                  //문자열 입력 받아서 str1에 저장
+            string str2 = Console.ReadLine();                                  //비교할 문자열 입력받아 str2에 저장
+            int a = str1.IndexOf(str2);                                        // IndexOf 함수를 사용해서 비교하여 가장첫번째 인덱스 반환받아 a에 집어넣음
+            if (a == -1)                                                       // a가 -1일 경우 일치하는 문자열이 없다는 것이므로 예외처리
+            {
+                Console.WriteLine("해당 문자열에서는 문자를 찾을수 없습니다.");
+            }
+                Console.WriteLine(a);                                          // -1이 아닐경우 정답 인덱스인 a값 반환
+        }
+    }
+}
