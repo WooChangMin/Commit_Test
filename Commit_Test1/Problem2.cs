79b52c39041f6ad010c54839e0033913f24e057a
woochangmin <rebuilding33@gmail.com>
2023-04-14 21:03:22 +0900

    feat:2번문제풀이 문자열배열사용 , 주석포함, solution

---
+++ Commit_Test1/Problem2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commit_Test1
+{
+    internal class Problem2
+    {
+        public void Test2()
+        {
+            string str = Console.ReadLine();                    //문자열을 입력받기위한 readline커맨드사용
+            string[] words = str.Split(' ');                    //string배열에 스페이스바를 기준으로 나눈 문자열들을 저장함
+            Console.WriteLine(words.Length);                    //words 문자열의 길이를 출력 >> 문장에서의 단어수
+        }
+
+    }
+
+}
